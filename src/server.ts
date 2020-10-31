a7e6e25d4259269e3a2da9c842d1e61d93ac57ca
Joel Fagundo <joel.fagundo@gmail.com>
2020-10-30 18:33:14 -0600

    netlify config

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+
+import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServer as ApolloServerLambda } from 'apollo-server-lambda'
+import { typeDefs } from './schema'
+import { resolvers } from './resolvers'
+import { todosRepo } from './modules/todos/repos';
+import { TodoRepo } from './modules/todos/repos/todoRepo';
+export type Context = { todosRepo: TodoRepo }
+
+
+
+function createLambdaServer () {
+  return new ApolloServerLambda({
+    context: () => ({ todosRepo } as Context),
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
+
+function createLocalServer () {
+  return new ApolloServer({
+    context: () => ({ todosRepo } as Context),
+    typeDefs,
+    resolvers,
+    introspection: true,
+    playground: true,
+  });
+}
+
+export { createLambdaServer, createLocalServer }
